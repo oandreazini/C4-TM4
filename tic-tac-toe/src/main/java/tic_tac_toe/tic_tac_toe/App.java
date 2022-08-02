2dd05b489859fdfe7bcf69cf4490c6a0e8f6a45f
oandreazini <oriol_as_92@hotmail.com>
2022-08-02 09:05:14 +0200

    Structure TicTacToe

---
+++ tic-tac-toe/src/main/java/tic_tac_toe/tic_tac_toe/App.java
@@ -0,0 +1,13 @@
+package tic_tac_toe.tic_tac_toe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
